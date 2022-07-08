f04ea8e33134d132047b64a0a6ca3bf7bab3b784
Davidlopotuha <goodmynick@gmail.com>
2022-07-08 16:47:06 +0300

    test

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+
+function test () {
+alert("Акторську кар'єру Карен Ґіллан почала з незначних ролей у багатьох серіалах.Перша головна роль припала на ситком Шоу Кевіна Бішопа (2008-2009). У цьому серіалі вона зіграла багатьох персонажів, у тому числі таких знаменитостей як Кеті Перрі та Анджеліна Джолі. Потім вона зіграла роль Джинні у фільмі каналу Channel 4 Stacked Також Ґіллан виконала головну роль у хоррор-проекті The Well, який виходив короткометражними епізодами на BBC Two та пізніше — вебепізодами на BBC.co.uk. У травні 2009 року її було обрано на роль супутниці Одинадцятого Доктора Емі Понд. Дебют Ґіллан у театрі відбувся 16 жовтня 2011 року, коли вона виконала роль Ширлі в п'єсі Джона Осборна «Неприпустимі докази».26 січня 2012 року Карен Ґіллан зіграла супермодель Джин Шрімптон у фільмі каналу BBC Four Ми підкоримо Мангеттен, який оповідає історію взаємин Шрімптон із фотографом Девідом Бейлі. ")
+}
+/*
+var clock = document.getElementById('clock');
+var color = document.getElementById('color');
+
+function timeClock (){
+  var time=new Date().toString();
+  var h = (time.getHours()% 12).toString();
+  var m = time.getMinutes.toString();
+  var s =time.getSeconds.toString();
+
+  if (h.length<2){
+    h='0'+h;
+  }
+  if(m.length<2){
+    m='0'+m;
+
+  }
+  if (s.length<2){
+
+  }
+  s='0'+s;
+{
+  var clockString=h+':'+m+":"+s;
+var colorString='#'+h+m+s;
+clock.textContent=clockString;
+color.textContent=colorString;
+}
+timeClock();
+setInterval(timeClock,1000);
+
+
+} 
+*/
